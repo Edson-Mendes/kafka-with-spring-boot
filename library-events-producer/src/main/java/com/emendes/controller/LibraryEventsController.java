4121c8f76d0c2ede2f1e74aae76903f5b2b7c99f
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-03-28 17:17:43 -0300

    Docker compose com Kafka cluster | Properties para configurar um producer

---
+++ library-events-producer/src/main/java/com/emendes/controller/LibraryEventsController.java
@@ -0,0 +1,28 @@
+package com.emendes.controller;
+
+import com.emendes.domain.LibraryEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@Slf4j
+public class LibraryEventsController {
+
+  private final Random random = new Random();
+
+  @PostMapping("/v1/libraryevent")
+  public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+
+    // Invoke Kafka Producer
+    log.info("A LibraryEvent was created!");
+    libraryEvent.setLibraryEventId(random.nextInt(100, 1000));
+    return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+  }
+
+}
