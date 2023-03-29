38e7a9b82da03fbcb4ccf3e3b7e990b09461c1d3
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-03-29 14:05:03 -0300

    Enviando mensagem de maneira assíncrona e síncrona

--- library-events-producer/src/main/java/com/emendes/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/emendes/controller/LibraryEventsController.java
@@ -1,27 +1,37 @@
 package com.emendes.controller;
 
 import com.emendes.domain.LibraryEvent;
+import com.emendes.producer.LibraryEventProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
+import java.util.concurrent.ExecutionException;
 
+@RequiredArgsConstructor
 @RestController
 @Slf4j
 public class LibraryEventsController {
 
-  private final Random random = new Random();
+  private final LibraryEventProducer libraryEventProducer;
 
   @PostMapping("/v1/libraryevents")
-  public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+  public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
 
-    // Invoke Kafka Producer
-    log.info("A LibraryEvent was created!");
-    libraryEvent.setLibraryEventId(random.nextInt(100, 1000));
+    log.info("Before sendLibraryEvent");
+
+//    libraryEventProducer.sendLibraryEvent(libraryEvent);
+    SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+    log.info("SendResult is {}", sendResult.toString());
+    log.info("After sendLibraryEvent");
+
+    log.info("A LibraryEvent was sent!");
     return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
   }
 
--- library-events-producer/src/main/java/com/emendes/producer/LibraryEventProducer.java
+++ library-events-producer/src/main/java/com/emendes/producer/LibraryEventProducer.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.ExecutionException;
+
 @RequiredArgsConstructor
 @Component
 @Slf4j
@@ -24,7 +26,7 @@ public class LibraryEventProducer {
     String value = mapper.writeValueAsString(libraryEvent);
 
     ListenableFuture<SendResult<Integer, String>> listenableFuture = kafkaTemplate.sendDefault(key, value);
-    listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+    listenableFuture.addCallback(new ListenableFutureCallback<>() {
       @Override
       public void onFailure(Throwable ex) {
         handleFailure(key, value, ex);
@@ -35,6 +37,24 @@ public class LibraryEventProducer {
         handleSuccess(key, value, result);
       }
     });
+  }
+
+  public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+    Integer key = libraryEvent.getLibraryEventId();
+    String value = mapper.writeValueAsString(libraryEvent);
+    SendResult<Integer, String> sendResult = null;
+
+    try {
+      sendResult = kafkaTemplate.sendDefault(key, value).get();
+    } catch (InterruptedException | ExecutionException ex) {
+      log.error("InterruptedException/ExecutionException sending the message and the exception is {}", ex.getMessage());
+      throw ex;
+    } catch (Exception ex) {
+      log.error("Exception sending the message and the exception is {}", ex.getMessage());
+      throw ex;
+    }
+
+    return sendResult;
   }
 
   private void handleFailure(Integer key, String value, Throwable ex) {
